0a78919b65b25e54c2d6fe951753e6302e7335a1
yadav Bakelal <bake2101992@gmail.com>
2023-11-17 16:19:06 +0530

    Update Flow-Control Part-1  Introduction.java

--- Flow-Control Part-1  Introduction.java
+++ Flow-Control Part-1  Introduction.java
@@ -1,26 +1,27 @@
 
-																		Flow-Control Part-1 || Introduction
-																	=========================================
+												Flow-Control Part-1 || Introduction
+											     =========================================
 
 --------------------
-	Flow-Control
+   Flow-Control
 --------------------
 
 	-> Flow-Control describes the order in which the statement will be executed at runtime.
 
 	
 
-															Flow-Control
-																|
+										Flow-Control
+  										     |
 				-------------------------------------------------------------------------------------------------------------
-				|												|														    |		
-				|												|															|
-		1. Selection Statement 						2. Iterative statement 									      3. Transfer Statement	
+				|							|						    |		
+				|							|						    |
+		1. Selection Statement 						2. Iterative statement 				      3. Transfer Statement	
 		
-		 1. if-else									  1. while 															1. break
-		 1. switch 									  2. do-while														2. continue 
-													  3. for()															3. return 
-													  4. for each loop(1.5v) 											4. try-catch-finally
-																														5. assert (1.4v)
+		 1. if-else							     1. while 							1. break
+		 1. switch 							     2. do-while						2. continue 
+								 		     3. for()							3. return 
+										     4. for each loop(1.5v) 					4. try-catch-finally
+																		5. assert (1.4v)
 																														
-																														
+
+													
